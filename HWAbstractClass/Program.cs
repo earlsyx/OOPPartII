d92a1d296c5d1dcfecbfa0a1883e8f1b8f8b84a3
Earl Syx Hernandez <hernandezearlsyxmail@gmail.com>
2025-07-23 07:04:26 +0800

    Home work, abstacr class that has interface  appleid to it, when isntatiaiting child class store it using the interfac type

---
+++ HWAbstractClass/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HWAbstractClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IVehicleParts vehicle = new Car();
+            vehicle.NumberOfWheels = 4;
+        }
+
+        
+    }
+    public interface IVehicleParts
+    {
+        int NumberOfWheels { get; set; }
+    }
+    public abstract class Vehicle : IVehicleParts
+    {
+        public int NumberOfWheels { get; set; }
+    }
+
+    public class Car : Vehicle
+    {
+
+    }
+}
