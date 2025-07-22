c5ea27eae2376a2ca06b7f1864381814adac44cd
Earl Syx Hernandez <hernandezearlsyxmail@gmail.com>
2025-07-22 08:16:09 +0800

    HW

---
+++ HWInterface/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HWInterface
+{
+    public class Person : IRun
+    {
+        public void Run()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HWInterface/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HWInterface
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<IRun> runs = new List<IRun>();
+            Person person = new Person();
+            Animal animal = new Animal();
+
+            runs.Add(person);
+            runs.Add(animal);
+
+            foreach (var run in runs)
+            {
+                Console.WriteLine($"{run}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
