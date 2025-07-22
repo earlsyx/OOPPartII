1c7caf0eebcf1bce0773e2268b14ce20644b84c1
Earl Syx Hernandez <hernandezearlsyxmail@gmail.com>
2025-07-22 23:07:52 +0800

    Tip - identify who needs an objet in order to understand which modifier it needs.
    public , private, internal,protected
    ,private protected, internal protected

---
+++ AccessModifierDemo/Program.cs
@@ -0,0 +1,52 @@
+﻿using DemoLibrary;
+using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifierDemo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+
+            //DataAccess data = new DataAccess();
+            //data.GetConnectionString();
+            person.SavePerson();
+            Console.ReadLine();
+        }
+    }
+
+    public class ModifiedDataAccess : DataAccess
+    {
+        public void GetUnsecureConnectionInfo()
+        {
+            GetConnectionString();
+        }
+    }
+    public class CEO : Manager
+    {
+        public void GetConnectionInfo()
+        {
+            //DataAccess data = new DataAccess();
+            //can't access even though you are inheriting from it
+            ModifiedDataAccess data = new ModifiedDataAccess();
+            data.GetUnsecureConnectionInfo();
+            formerLastName = "Test"; 
+
+            //security is not more so to secure your data
+            //more about saying these are the things we want to have you use, and these are the things we don't want you to use
+
+            //can i get around . tyes? not the point.
+            // when i say data. i only want things im allowed to do, filter aonly want you want.
+
+            // layer security, ui should not have access to connection string datacess so mark as internal.
+            // sss private, so nobody have access to creatae a screuity flaw, convenient security. not lock down , everytihn is totally secure.
+           
+        }
+    }
+}
---
+++ DemoLibrary/Person.cs
@@ -0,0 +1,92 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+   //privateprotected - you have access to something, if it's in a class or a derived class in a current assembly
+
+    //only for things inherit from person, but also the same assembly.
+    //anything class in the DemoLibrary /project/assembly
+    //you can have the class mark as public
+    // marks method as protected internal
+    public class DataAccess
+    {
+        protected internal string GetConnectionString()
+        {
+            return "sensitive";
+        }
+    }
+    public class Person
+    {
+        //public
+        //private 
+        // protected allows accessing in the class you created as well, any class that derives from this class, so child class.
+
+        // public private common,
+        // protected internal, good value, undrastnd hwo.
+        // change later on, protected first etc.
+
+        //public private, then iwant it to be vaible not anywhere or everwher etc.
+        //just know their avialabe
+        private protected string formerLastName = "";
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+
+        private string _ssn;//not returning the actual variable
+        // we are returning modified version of it 
+        //  public versino returns modifed of private//internal only
+
+        public string SSN
+        {
+            get 
+            { 
+                return $"***-**-{_ssn.Substring(_ssn.Length - 4)}"; 
+            }
+            set 
+            { 
+                _ssn = value; 
+            }
+        }
+
+        public void ChangeLastName(string newLastName)
+        {
+            formerLastName = LastName;
+            LastName = newLastName;
+        }
+
+        public void SayHello()
+        {
+            string test = "Hi";
+;            Console.WriteLine($"Hello { FirstName }");
+        }
+
+        public void SavePerson()
+        {
+            DataAccess data = new DataAccess();
+            string conn = data.GetConnectionString();
+            // saves the person
+        }
+    }
+
+    public class Employee : Person
+    {
+         public string GetFormerLastName()
+        {
+            return formerLastName;
+        }
+    }
+
+    public class Manager : Employee
+    {
+        public string GetAllNames()
+        {
+            return $"{FirstName}, {LastName}, {formerLastName}";
+        }
+    }
+
+
+}
